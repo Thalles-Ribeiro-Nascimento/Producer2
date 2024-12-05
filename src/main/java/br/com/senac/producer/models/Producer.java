17d4e831b30a185a3e25de14abf8fbe9ff4a7921
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2024-12-05 07:54:40 -0300

    Primeiro commit

---
+++ src/main/java/br/com/senac/producer/models/Producer.java
@@ -0,0 +1,34 @@
+package br.com.senac.producer.models;
+
+import java.io.Serializable;
+
+public class Producer implements Serializable {
+
+    private String userStatus;
+    private String nameUser;
+    private String emailUser;
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+}
